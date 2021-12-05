ab50a45b220438b3583cb30a4c1da64395e00c47
kwwangkw <kwwangkw@umich.edu>
2021-12-04 21:40:49 -0500

    for if for example

---
+++ LoopFusePrePass/workfiles/foriffor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    for(int i = 0; i < 10; i++){
+        int j = i;
+    }
+    if (true) {
+        for(int i = 0; i < 10; i++){
+        int k = i;
+    }
+    }
+}
