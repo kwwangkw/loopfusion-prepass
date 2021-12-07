0388597e472937307bcebee09795bb4b8a41ae3b
James Wu <wujames@eecs583b.eecs.umich.edu>
2021-12-06 22:52:19 -0500

    added foriffor 3 and 4

---
+++ LoopFusePrePass/workfiles/foriffor4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main(){
+    int a = rand() % 10 + 1;
+    for(int i = 0; i < 10; i++){
+        for(int x = 0; x < 10; i++) {
+            int j = i;
+        }
+    }
+    if (a == 2) {
+        for(int i = 0; i < 10; i++){
+            for(int x = 0; x < 10; i++) {
+                int k = i;
+            }
+        }
+    }
+    return 0;
+}
